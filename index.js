ec730ce31b71678668438a523276c8773931fc21
Gouacide <25200718+Gouacide@users.noreply.github.com>
2019-11-04 14:45:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
   })
 
+app.get('/user/:name', function(req, res) {
+	var age=''+req.query.age;
+	if(age!=="undefined" && age.trim().length){
+	res.send('Hello '  + req.params.name + ' tu as ' + age +' ans');
+	}else{
+	(res.send('Hello '  + req.params.name));
+	}
+})
+
+
+
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-  })
+  });
+  
